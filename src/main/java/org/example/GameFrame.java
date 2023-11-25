29c83f002f3d04eabe490b4f6973724215b8330b
Aleksandra Gorska <aleksandra.gorska02@gmail.com>
2023-11-25 19:47:10 +0100

    snake with one snake

--- src/main/java/org/example/GameFrame.java
+++ src/main/java/org/example/GameFrame.java
@@ -1,4 +1,18 @@
 package org.example;
 
-public class GameFrame {
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+        GameFrame(){
+
+                this.add(new GamePanel());
+                this.setTitle("snake");
+                this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                this.setResizable(false);
+                this.pack();
+                this.setVisible(true);
+                this.setLocationRelativeTo(null);
+                }
 }
--- src/main/java/org/example/SnakeGame.java
+++ src/main/java/org/example/SnakeGame.java
@@ -3,8 +3,8 @@ package org.example;
 public class SnakeGame {
     public static void main(String[] args) {
 
+new GameFrame();
 
 
-        System.out.println("Hello world!");
     }
 }
