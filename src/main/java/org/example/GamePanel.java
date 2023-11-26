a48248f0a46393e48c2afdeb3458d7c78939df19
Aleksandra Gorska <aleksandra.gorska02@gmail.com>
2023-11-26 23:19:22 +0100

    change position

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -3,6 +3,7 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Objects;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
@@ -17,42 +18,27 @@ public class GamePanel extends JPanel implements ActionListener {
     Snake pl2 = new Snake();
     private String winner;
 
-    public void setPl1(Snake pl1) {
-        this.pl1 = pl1;
-        pl1.appleEaten = 0;
-        pl1.bodyParts = 6;
-        pl1.direction = 'R';
-        pl1.color = "red";
-
-
-    }
-    public void setPl2(Snake pl2) {
-        this.pl2 = pl2;
-        pl2.appleEaten = 0;
-        pl2.bodyParts = 6;
-        pl2.direction = 'D';
-        pl2.color = "blue";
-
-    }
-
-
-
-
-
+//set p1 start position
 
     static int appleX;
     static int appleY;
 
-   public static boolean running = false;
+    public static boolean running = false;
     Timer timer;
     static Random random;
 
     GamePanel() {
         random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
-        this.setBackground(Color.black);
+        this.setBackground(Color.darkGray);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
+        pl1.x[0] = 0;
+        pl1.y[0] = 0;
+        pl1.direction = 'R';
+        pl2.x[0] = SCREEN_WIDTH - UNIT_SIZE;
+        pl2.y[0] = SCREEN_HEIGHT - UNIT_SIZE;
+        pl2.direction = 'L';
         startGame();
     }
 
@@ -71,7 +57,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void draw(Graphics g) {
 
-        if(running) {
+        if (running) {
           /*
           for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
                 g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
@@ -87,23 +73,15 @@ public class GamePanel extends JPanel implements ActionListener {
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
-
-
-            g.setColor(Color.red);
-            g.setFont(new Font("Ink Free",Font.BOLD,30));
-            FontMetrics metrics=getFontMetrics(g.getFont());
-            g.drawString("Score "+pl1.appleEaten,(SCREEN_WIDTH-metrics.stringWidth("Score "+pl1.appleEaten))/2,g.getFont().getSize());
-
-        }
-        else {
+        } else {
             gameOver(g);
         }
     }
 
     public static void newApple() {
 
-        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
-        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+        appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
 
 
     }
@@ -116,22 +94,22 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkApple() {
-          pl1.checkAppleSnake();
-          pl2.checkAppleSnake();
+        pl1.checkAppleSnake();
+        pl2.checkAppleSnake();
     }
 
     public void checkColisions() {
 
-       pl1.checkCollisionsSnake();
-       if(!running){
-           winner = "Player 2";
-       }
-       pl2.checkCollisionsSnake();
-         if(!running){
-              winner = "Player 1";
-         }
-       //check if head pl1 touches pl2
-        for (int i=pl2.bodyParts;i>0;i--){
+        pl1.checkCollisionsSnake();
+        if (!running) {
+            winner = "Player 2";
+        }
+        pl2.checkCollisionsSnake();
+        if (!running && !Objects.equals(winner, "Player 2")) {
+            winner = "Player 1";
+        }
+        //check if head pl1 touches pl2
+        for (int i = pl2.bodyParts; i > 0; i--) {
             if ((pl1.x[0] == pl2.x[i]) && (pl1.y[0] == pl2.y[i])) {
                 running = false;
                 winner = "Player 2";
@@ -139,14 +117,14 @@ public class GamePanel extends JPanel implements ActionListener {
             }
         }
         //check if head pl2 touches pl1
-        for (int i=pl1.bodyParts;i>0;i--){
+        for (int i = pl1.bodyParts; i > 0; i--) {
             if ((pl2.x[0] == pl1.x[i]) && (pl2.y[0] == pl1.y[i])) {
                 running = false;
                 winner = "Player 1";
                 break;
             }
         }
-        if(!running){
+        if (!running) {
             timer.stop();
         }
 
@@ -155,15 +133,15 @@ public class GamePanel extends JPanel implements ActionListener {
     public void gameOver(Graphics g) {
 
         g.setColor(Color.red);
-        g.setFont(new Font("Ink Free",Font.BOLD,75));
-        FontMetrics metrics1=getFontMetrics(g.getFont());
-        g.drawString("GAME OVER",(SCREEN_WIDTH-metrics1.stringWidth("GAME OVER"))/2,SCREEN_HEIGHT/2);
-        g.drawString("Winner: "+winner,(SCREEN_WIDTH-metrics1.stringWidth("GAME OVER"))/2,SCREEN_HEIGHT/3);
+        g.setFont(new Font("Arial", Font.BOLD, 50));
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("GAME OVER", (SCREEN_WIDTH - metrics1.stringWidth("GAME OVER")) / 2, SCREEN_HEIGHT / 2);
+        g.drawString("Winner: " + winner, (SCREEN_WIDTH - metrics1.stringWidth("Winner: " + winner)) / 2, SCREEN_HEIGHT / 3);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(running){
+        if (running) {
             move();
             checkApple();
             checkColisions();
@@ -175,45 +153,45 @@ public class GamePanel extends JPanel implements ActionListener {
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-            switch (e.getKeyCode()){
+            switch (e.getKeyCode()) {
                 case KeyEvent.VK_LEFT:
-                    if(pl1.direction !='R'){
+                    if (pl1.direction != 'R') {
                         pl1.direction = 'L';
                     }
                     break;
                 case KeyEvent.VK_RIGHT:
-                    if(pl1.direction !='L'){
+                    if (pl1.direction != 'L') {
                         pl1.direction = 'R';
                     }
                     break;
                 case KeyEvent.VK_DOWN:
-                    if(pl1.direction !='U'){
+                    if (pl1.direction != 'U') {
                         pl1.direction = 'D';
                     }
                     break;
                 case KeyEvent.VK_UP:
-                    if(pl1.direction !='D'){
+                    if (pl1.direction != 'D') {
                         pl1.direction = 'U';
                     }
                     break;
 
                 case KeyEvent.VK_A:
-                    if(pl2.direction !='R'){
+                    if (pl2.direction != 'R') {
                         pl2.direction = 'L';
                     }
                     break;
                 case KeyEvent.VK_D:
-                    if(pl2.direction !='L'){
+                    if (pl2.direction != 'L') {
                         pl2.direction = 'R';
                     }
                     break;
                 case KeyEvent.VK_S:
-                    if(pl2.direction !='U'){
+                    if (pl2.direction != 'U') {
                         pl2.direction = 'D';
                     }
                     break;
                 case KeyEvent.VK_W:
-                    if(pl2.direction !='D'){
+                    if (pl2.direction != 'D') {
                         pl2.direction = 'U';
                     }
                     break;
--- src/main/java/org/example/Snake.java
+++ src/main/java/org/example/Snake.java
@@ -8,14 +8,12 @@ import static org.example.GamePanel.UNIT_SIZE;
 public class Snake {
     int bodyParts = 6;
     int appleEaten;
-    public char direction='R';
-    final int x[] = new int[GAME_UNITS];
-    final int y[] = new int[GAME_UNITS];
+    public char direction = 'R';
+    final int[] x = new int[GAME_UNITS];
+    final int[] y = new int[GAME_UNITS];
 
 
-    String color;
-
-    public void drawSnake(Graphics g){
+    public void drawSnake(Graphics g) {
         for (int i = 0; i < this.bodyParts; i++) {
             if (i == 0) {
 
@@ -26,56 +24,60 @@ public class Snake {
             }
         }
     }
-    public void moveSnake(){
-        for (int i=bodyParts;i>0;i--){
-            x[i]=x[i-1];
-            y[i]=y[i-1];
+
+    public void moveSnake() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
         }
-        switch (direction){
+        switch (direction) {
             case 'U':
-                y[0]=y[0]-UNIT_SIZE;
+                y[0] = y[0] - UNIT_SIZE;
                 break;
             case 'D':
-                y[0]=y[0]+UNIT_SIZE;
+                y[0] = y[0] + UNIT_SIZE;
                 break;
             case 'L':
-                x[0]=x[0]-UNIT_SIZE;
+                x[0] = x[0] - UNIT_SIZE;
                 break;
             case 'R':
-                x[0]=x[0]+UNIT_SIZE;
+                x[0] = x[0] + UNIT_SIZE;
                 break;
         }
     }
-    public void checkAppleSnake(){
-        if ((x[0]==GamePanel.appleX)&&(y[0]==GamePanel.appleY)){
+
+    public void checkAppleSnake() {
+        if ((x[0] == GamePanel.appleX) && (y[0] == GamePanel.appleY)) {
             bodyParts++;
             appleEaten++;
             GamePanel.newApple();
         }
 
     }
-    public void checkCollisionsSnake(){
+
+    public void checkCollisionsSnake() {
         //checks if head collides with body
-        for (int i=bodyParts;i>0;i--){
-            if ((x[0]==x[i])&&(y[0]==y[i])){
-                GamePanel.running=false;
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                GamePanel.running = false;
+                break;
             }
         }
         //check if head touches left border
-        if (x[0]<0){
-            GamePanel.running=false;
+        if (x[0] < 0) {
+            GamePanel.running = false;
         }
         //check if head touches right border
-        if (x[0]>GamePanel.SCREEN_WIDTH){
-            GamePanel.running=false;
+        if (x[0] > GamePanel.SCREEN_WIDTH) {
+            GamePanel.running = false;
         }
         //check if head touches top border
-        if (y[0]<0){
-            GamePanel.running=false;
+        if (y[0] < 0) {
+            GamePanel.running = false;
         }
         //check if head touches bottom border
-        if (y[0]>GamePanel.SCREEN_HEIGHT){
-            GamePanel.running=false;
+        if (y[0] > GamePanel.SCREEN_HEIGHT) {
+            GamePanel.running = false;
         }
 
     }
